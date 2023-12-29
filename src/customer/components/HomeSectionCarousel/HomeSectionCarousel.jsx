ec3ac66ca3d6e4e99521dc5a5f96239b74e69ec6
tonnymuchui <tonykanyingah@gmail.com>
2023-12-29 23:56:21 +0300

    Add homesectioncarousel slide

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,60 +1,82 @@
-import React from "react";
+import { React, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import { Button } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { mens_kurta } from "../../../Data/men_kurta";
 
 const HomeSectionCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+
   const responsive = {
-    0: { items: 2 },
+    0: { items: 1 },
     720: { items: 3 },
-    1024: { items: 5.5 },
+    1024: { items: 5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const renderNextButton = ({ isDisabled }) => {
+    if (activeIndex !== items.length - 5) {
+    
+    return (
+      <Button
+        variant="contained"
+        className="z-50 bg-white"
+        sx={{
+          position: "absolute",
+          top: "8rem",
+          right: "-1.3rem",
+          transform: "translateX(50%) rotate(90deg)",
+          bgcolor: "white",
+        }}
+        aria-label="next"
+      >
+        <KeyboardArrowLeftIcon
+          sx={{ transform: "rotate(90deg)", color: "black" }}
+        />
+      </Button>);
+    }
+  };
+
+  const renderPrevButton = ({ isDisabled }) => {
+    if (activeIndex !== 0) {
+    return (<Button
+      variant="contained"
+      bg-color="white"
+      className="z-50"
+      sx={{
+        position: "absolute",
+        top: "8rem",
+        left: "-1.3rem",
+        transform: "translateX(-50%) rotate(-90deg)",
+        bgcolor: "white",
+      }}
+      aria-label="next"
+    >
+      <KeyboardArrowRightIcon
+        sx={{ transform: "rotate(-90deg)", color: "black" }}
+      />
+    </Button>);
+    }
+  };
+
+  const items = mens_kurta.slice(0, 10).map((item) => <HomeSectionCard product={item} />);
   return (
     <div className="">
       <div className="relative p-5 border">
         <AliceCarousel
           items={items}
-          disableButtonsControls
-          infinite
           responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+          renderNextButton={renderNextButton}
+          renderPrevButton={renderPrevButton}
         />
-        <Button
-          variant="contained"
-          bg-color="white"
-          className="z-50"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            right: "0rem",
-            transform:"translateX(50%) rotate(90deg)",
-            bgcolor: "white"
-          }}
-          aria-label="next"
-        >
-          <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)",color: "black"}}/>
-        </Button>
-
-        <Button
-          variant="contained"
-          bg-color="white"
-          className="z-50"
-          sx={{
-            position: "absolute",
-            top: "8rem",
-            left: "0rem",
-            transform:"translateX(-50%) rotate(-90deg)",
-            bgcolor: "white"
-          }}
-          aria-label="next"
-        >
-          <KeyboardArrowRightIcon sx={{transform:"rotate(-90deg)",color: "black"}}/>
-        </Button>
       </div>
     </div>
   );
