5c3538089ec6808e40b0b410f234649a17619d48
tonnymuchui <tonykanyingah@gmail.com>
2024-01-04 05:15:28 +0300

    Update maincarouseldata

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -1,14 +1,22 @@
-export const MainCarouselData = [
+export const mainCarouselData = [
     {
-        image: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-        path: "woman/clothing/lengha_choli"
+    image:
+    "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3//3_9.jpg",
+    path: "/women/clothing/lengha_choli",
     },
     {
-        image: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-        path: "woman/clothing/women_dress"
+    image:
+    "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4//4_8.jpg",
+    path: "/women/clothing/women_dress",
     },
     {
-        image: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
-        path: "woman/clothing/women_dress"
-    }
-]
+    image:
+    "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1//1_5.jpg",
+    path: "/women/clothing/women_dress",
+    },
+    {
+    image:
+    "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2//2_9.jpg",
+    path: "/women/clothing/women_saree",
+    },
+    ];
