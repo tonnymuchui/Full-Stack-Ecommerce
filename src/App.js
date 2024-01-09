f5d89bb11ad167dd5e0e8707acc864cfa17fdecd
tonnymuchui <tonykanyingah@gmail.com>
2024-01-09 03:46:53 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CheckOut from './customer/components/Checkout/CheckOut';
+import Order from './customer/components/Order/Order';
+import OrderDetails from './customer/components/Order/OrderDetails';
 import Footer from './customer/components/footer/Footer';
 import Navbar from './customer/components/navigation/NavBar';
-import ProductDetails from './customer/components/productDetails.jsx/ProductDetails';
+import CustomerRouters from './Routers/CustomerRouters';
+// import ProductDetails from './customer/components/productDetails.jsx/ProductDetails';
 // import HomePage from './customer/pages/HomePage/HomePage';
 
 function App() {
   return (
-    <>
     <div className="">
-      <Navbar />
+      <Routes>
+        <Route path='/*' element={<CustomerRouters />}></Route>
+      </Routes>
     </div>
-    <div>
-      {/* <HomePage /> */}
-      {/* <Product /> */}
-      <ProductDetails />
-    </div>
-    <div>
-      <Footer />
-    </div>
-    </>
   );
 }
 
