0b9932e334c9ab57052a051b946adf5449f934b5
tonnymuchui <tonykanyingah@gmail.com>
2024-01-09 03:48:53 +0300

    Add Order

--- src/customer/components/Order/OrderCard.jsx
+++ src/customer/components/Order/OrderCard.jsx
@@ -1,10 +1,13 @@
 import { AdjustOutlined } from "@mui/icons-material";
 import { Grid } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const OrderCard = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="p-5 shadow-md shadow-black hover:shadow-2xl border">
+    <div onClick={() => navigate(`/account/order/${5}`)} className="p-5 shadow-md shadow-black hover:shadow-2xl border">
       <Grid container spacing={2} sx={{ justifyContent: "space-between" }}>
         <Grid item xs={6}>
           <div className="flex cursor-pointer">
