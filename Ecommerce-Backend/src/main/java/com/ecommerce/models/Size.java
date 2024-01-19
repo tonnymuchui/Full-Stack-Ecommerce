a8718cefa1da034c5cdf371720a5c39f9fa89a10
tonnymuchui <tonykanyingah@gmail.com>
2024-01-19 03:43:48 +0300

    Add Size model

---
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/Size.java
@@ -0,0 +1,26 @@
+package com.ecommerce.models;
+
+public class Size {
+    private String name;
+    private String quantity;
+
+    public Size(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
