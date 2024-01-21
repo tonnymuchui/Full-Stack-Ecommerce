6ea7489dd9ab1ffbdcc5d7a4366de2bc9c4bc9b8
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:10:32 +0300

    Update User model to DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/models/User.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/User.java
@@ -2,12 +2,17 @@ package com.ecommerce.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User {
     @Id
@@ -20,10 +25,10 @@ public class User {
     private String email;
     private String role;
     private String mobile;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Address> addresses = new ArrayList<>();
 
-    @Embedded
     @ElementCollection
     @CollectionTable(name = "payment_information", joinColumns = @JoinColumn(name = "user_id"))
     private List<PaymentInformation> paymentInformation = new ArrayList<>();
@@ -37,120 +42,4 @@ public class User {
     private List<Review> reviews = new ArrayList<>();
 
     private LocalDateTime localDateTime;
-
-    public User() {
-
-    }
-
-    public User(Long id, String firstName, String lastName, String password, String email, String role, String mobile, List<Address> addresses, List<PaymentInformation> paymentInformation, List<Rating> ratings, List<Review> reviews, LocalDateTime localDateTime) {
-        super();
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.email = email;
-        this.role = role;
-        this.mobile = mobile;
-        this.addresses = addresses;
-        this.paymentInformation = paymentInformation;
-        this.ratings = ratings;
-        this.reviews = reviews;
-        this.localDateTime = localDateTime;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public List<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
-    }
-
-    public List<PaymentInformation> getPaymentInformation() {
-        return paymentInformation;
-    }
-
-    public void setPaymentInformation(List<PaymentInformation> paymentInformation) {
-        this.paymentInformation = paymentInformation;
-    }
-
-    public List<Rating> getRatings() {
-        return ratings;
-    }
-
-    public void setRatings(List<Rating> ratings) {
-        this.ratings = ratings;
-    }
-
-    public List<Review> getReviews() {
-        return reviews;
-    }
-
-    public void setReviews(List<Review> reviews) {
-        this.reviews = reviews;
-    }
-
-    public LocalDateTime getLocalDateTime() {
-        return localDateTime;
-    }
-
-    public void setLocalDateTime(LocalDateTime localDateTime) {
-        this.localDateTime = localDateTime;
-    }
 }
