0869c580a11dd21094733434020a888466e7474f
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:08:51 +0300

    Add LoginRequest DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/request/LoginRequest.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/request/LoginRequest.java
@@ -1,32 +1,13 @@
 package com.ecommerce.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
-
-    public LoginRequest(){
-
-    }
-
-    public LoginRequest(String email, String password) {
-        super();
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
