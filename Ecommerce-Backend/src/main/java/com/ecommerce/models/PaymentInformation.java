bc4a14971d9b92a2f2963fbe3ffa0cde9f5fd60d
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:09:16 +0300

    Add PaymentInformation model DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/models/PaymentInformation.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/PaymentInformation.java
@@ -2,17 +2,26 @@ package com.ecommerce.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class PaymentInformation {
     @Column(name = "cardholder_name")
     private String cardholderName;
+
     @Column(name = "card_number")
     private String cardNumber;
+
     @Column(name = "expiration_date")
-    private LocalDate localDate;
+    private LocalDate expirationDate;
+
     @Column(name = "cvv")
     private String cvv;
 }
