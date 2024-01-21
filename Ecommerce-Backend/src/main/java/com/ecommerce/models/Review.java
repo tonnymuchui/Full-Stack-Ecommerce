f38ad2a7c04357d36a138fddc7a2f7a276650cff
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:10:12 +0300

    Update Review model to DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/models/Review.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/Review.java
@@ -2,74 +2,31 @@ package com.ecommerce.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String review;
+
     @ManyToOne
     @JoinColumn(name = "product_id")
     @JsonIgnore
     private Product product;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
     private LocalDateTime createdAt;
-
-    public Review(){
-
-    }
-
-    public Review(Long id, String review, Product product, User user, LocalDateTime createdAt) {
-        super();
-        this.id = id;
-        this.review = review;
-        this.product = product;
-        this.user = user;
-        this.createdAt = createdAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getReview() {
-        return review;
-    }
-
-    public void setReview(String review) {
-        this.review = review;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
