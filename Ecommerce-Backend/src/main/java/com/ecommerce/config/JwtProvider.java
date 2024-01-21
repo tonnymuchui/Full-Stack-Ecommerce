faf4d512586f797b76641f2ab8fb7ae5f782d94d
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 01:11:55 +0300

    Add component annotation

--- Ecommerce-Backend/src/main/java/com/ecommerce/config/JwtProvider.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/config/JwtProvider.java
@@ -3,26 +3,30 @@ package com.ecommerce.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@Component
 public class JwtProvider {
-    public String generateToken(Authentication auth){
+    private static final long EXPIRATION_TIME = 846000000;
+
+    public String generateToken(Authentication auth) {
         SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
-        String jwt = Jwts.builder()
+        return Jwts.builder()
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime()+846000000))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .claim("email", auth.getName())
                 .signWith(key).compact();
-        return jwt;
     }
-    public String getEmailFromToken(String jwt){
+
+    public String getEmailFromToken(String jwt) {
         jwt = jwt.substring(7);
         SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
         Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-        String email = String.valueOf(claims.get("email"));
-        return email;
+        return String.valueOf(claims.get("email"));
     }
 }
