8957229f7148e60d9b2d37e51deabf4bba4a572b
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:10:23 +0300

    Update Size model to DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/models/Size.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/Size.java
@@ -1,26 +1,13 @@
 package com.ecommerce.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Size {
     private String name;
     private String quantity;
-
-    public Size(){
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(String quantity) {
-        this.quantity = quantity;
-    }
 }
