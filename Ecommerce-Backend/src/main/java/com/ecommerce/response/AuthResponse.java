1f45d3bfad27e889c069b55cdb1b8a9a2d66f7a2
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 01:08:39 +0300

    Add empty constructor

--- Ecommerce-Backend/src/main/java/com/ecommerce/response/AuthResponse.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/response/AuthResponse.java
@@ -4,6 +4,9 @@ public class AuthResponse {
     private String jwt;
     private String message;
 
+    public AuthResponse(){
+
+    }
     public AuthResponse(String jwt, String message) {
         super();
         this.jwt = jwt;
