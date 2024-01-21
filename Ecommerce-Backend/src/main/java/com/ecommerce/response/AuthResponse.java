17f13c7a7a4efe5df97eb69972edfea9910a1ccf
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:08:24 +0300

    Add AuthResponse DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/response/AuthResponse.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/response/AuthResponse.java
@@ -1,31 +1,13 @@
 package com.ecommerce.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String jwt;
     private String message;
-
-    public AuthResponse(){
-
-    }
-    public AuthResponse(String jwt, String message) {
-        super();
-        this.jwt = jwt;
-        this.message = message;
-    }
-
-    public String getJwt() {
-        return jwt;
-    }
-
-    public void setJwt(String jwt) {
-        this.jwt = jwt;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
