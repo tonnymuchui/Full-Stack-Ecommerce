7c331555e95a4097e6ee570dc73277f4348e4f59
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:08:37 +0300

    Add Category DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/models/Category.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/Category.java
@@ -3,7 +3,13 @@ package com.ecommerce.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Category {
     @Id
@@ -17,49 +23,6 @@ public class Category {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "parent_category_id")
     private Category parentCategory;
+
     private int level;
-
-    public Category() {
-
-    }
-
-    public Category(Long id,@NotNull @Size(max = 50) String name, Category parentCategory, int level) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.parentCategory = parentCategory;
-        this.level = level;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category getParentCategory() {
-        return parentCategory;
-    }
-
-    public void setParentCategory(Category parentCategory) {
-        this.parentCategory = parentCategory;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
 }
