1e3017192a0a4a42ffc5e62f99064029ebe7fb1c
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 01:12:53 +0300

    Add validate&extract function

--- Ecommerce-Backend/src/main/java/com/ecommerce/config/JwtValidator.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/config/JwtValidator.java
@@ -8,29 +8,29 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.util.List;
 
+@Component
 public class JwtValidator extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+        String jwt = extractJwtFromHeader(request);
         if (jwt != null) {
-            jwt = jwt.substring(7);
             try {
-                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
-                Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-
+                Claims claims = validateToken(jwt);
                 String email = String.valueOf(claims.get("email"));
                 String authorities = String.valueOf(claims.get("authorities"));
                 List<GrantedAuthority> grantedAuthorities = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
@@ -42,5 +42,15 @@ public class JwtValidator extends OncePerRequestFilter {
             }
         }
         filterChain.doFilter(request, response);
+    }
+
+    private String extractJwtFromHeader(HttpServletRequest request) {
+        String header = request.getHeader(JwtConstant.JWT_HEADER);
+        return (header != null && header.startsWith("Bearer ")) ? header.substring(7) : null;
+    }
+
+    private Claims validateToken(String jwt) {
+        SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+        return Jwts.parser().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
     }
 }
