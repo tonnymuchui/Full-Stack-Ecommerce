5c3f4a20f1a38bda7771e4d96c14492a9acc12ad
tonnymuchui <tonykanyingah@gmail.com>
2024-01-22 02:09:53 +0300

    Update Rating model to DI

--- Ecommerce-Backend/src/main/java/com/ecommerce/models/Rating.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/Rating.java
@@ -2,14 +2,22 @@ package com.ecommerce.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
@@ -18,6 +26,7 @@ public class Rating {
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
+
     @Column(name = "rating")
     private double rating;
 
