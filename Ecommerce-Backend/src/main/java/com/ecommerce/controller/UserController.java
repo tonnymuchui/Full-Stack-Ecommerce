bd21a4de60d387b4dfa30edd56d09a445334f38d
tonnymuchui <tonykanyingah@gmail.com>
2024-02-12 12:00:35 +0300

    Add UserController

---
+++ Ecommerce-Backend/src/main/java/com/ecommerce/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.exception.UserException;
+import com.ecommerce.models.User;
+import com.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfileHandler(@RequestHeader("Authorization") String jwt) throws UserException {
+        User user = userService.findUserProfileByJwt(jwt);
+        return new ResponseEntity<>(user, HttpStatus.ACCEPTED);
+    }
+}
