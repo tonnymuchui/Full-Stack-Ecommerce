cedcb187752f05d6f56e80c1867af57b07ace5a6
tonnymuchui <tonykanyingah@gmail.com>
2024-02-12 12:01:30 +0300

    Update UserService

--- Ecommerce-Backend/src/main/java/com/ecommerce/service/UserService.java
+++ Ecommerce-Backend/src/main/java/com/ecommerce/service/UserService.java
@@ -4,6 +4,7 @@ import com.ecommerce.exception.UserException;
 import com.ecommerce.models.User;
 
 public interface UserService {
+
     public User findUserById(Long userId) throws UserException;
     public User findUserProfileByJwt(String jwt) throws UserException;
 }
