75a0a2e64163666ce75f62a8a1ac7b02cd3b6c7c
tonnymuchui <tonykanyingah@gmail.com>
2024-02-12 12:06:33 +0300

    Add OrderItem model

---
+++ Ecommerce-Backend/src/main/java/com/ecommerce/models/OrderItem.java
@@ -0,0 +1,34 @@
+package com.ecommerce.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+    private String size;
+    private int quantity;
+    private Integer price;
+    private Integer discountedPrice;
+    private Long userId;
+    private LocalDateTime deliveryDate;
+
+}
