ddf1b41923aec0af54e9c298b341275e74eecee6
tonnymuchui <tonykanyingah@gmail.com>
2024-02-12 12:02:43 +0300

    Add RatingServiceImplementation

---
+++ Ecommerce-Backend/src/main/java/com/ecommerce/service/RatingServiceImplementation.java
@@ -0,0 +1,39 @@
+package com.ecommerce.service;
+
+import com.ecommerce.exception.ProductException;
+import com.ecommerce.models.Product;
+import com.ecommerce.models.Rating;
+import com.ecommerce.models.User;
+import com.ecommerce.repository.RatingRepository;
+import com.ecommerce.request.RatingRequest;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class RatingServiceImplementation implements RatingService{
+    private RatingRepository ratingRepository;
+    private ProductService productService;
+
+    public RatingServiceImplementation(RatingRepository ratingRepository, ProductService productService) {
+        this.ratingRepository = ratingRepository;
+        this.productService = productService;
+    }
+
+    @Override
+    public Rating createRating(RatingRequest req, User user) throws ProductException {
+        Product product = productService.findProductById(req.getProductId());
+        Rating rating = new Rating();
+        rating.setProduct(product);
+        rating.setUser(user);
+        rating.setRating(req.getRating());
+        rating.setCreatedAt(LocalDateTime.now());
+        return ratingRepository.save(rating);
+    }
+
+    @Override
+    public List<Rating> getProductsRating(Long productId) {
+        return ratingRepository.getAllProductsRating(productId);
+    }
+}
