5eaa88ae7162486e1922acdd0a236bba0de59c3d
tonnymuchui <tonykanyingah@gmail.com>
2024-02-25 03:11:13 +0300

    Update actions

--- Ecommerce-Frontend/src/State/Auth/Action.js
+++ Ecommerce-Frontend/src/State/Auth/Action.js
@@ -1,40 +1,42 @@
 import axios from "axios";
 import { API_BASE_URL } from "../../config/ApiConfig";
-import { LOGOUT } from "./ActionType";
+import { GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType";
 
 
-const REGISTER_REQUEST = () => ({type: REGISTER_REQUEST});
-const REGISTER_SUCCESS = (user) => ({type: REGISTER_SUCCESS,payload:user});
-const REGISTER_FAILURE = (err) => ({type: REGISTER_FAILURE,payload:err});
+// Action creator functions
+const registerRequest = () => ({ type: REGISTER_REQUEST });
+const registerSuccess = (user) => ({ type: REGISTER_SUCCESS, payload: user });
+const registerFailure = (err) => ({ type: REGISTER_FAILURE, payload: err });
 
-const LOGIN_REQUEST = () => ({type: LOGIN_REQUEST});
-const LOGIN_SUCCESS = (user) => ({type: LOGIN_SUCCESS,payload:user});
-const LOGIN_FAILURE = (err) => ({type: LOGIN_FAILURE,payload:err});
+const loginRequest = () => ({type: LOGIN_REQUEST});
+const loginSuccess = (user) => ({type: LOGIN_SUCCESS,payload:user});
+const loginFailure = (err) => ({type: LOGIN_FAILURE,payload:err});
 
-const GET_USER_REQUEST = () => ({type: GET_USER_REQUEST});
-const GET_USER_SUCCESS = (user) => ({type:GET_USER_SUCCESS,payload:user});
-const GET_USER_FAILURE = (err) => ({type:GET_USER_FAILURE,payload:err});
+const getUserRequest = () => ({type: GET_USER_REQUEST});
+const getUserSuccess = (user) => ({type:GET_USER_SUCCESS,payload:user});
+const getUserFailure = (err) => ({type:GET_USER_FAILURE,payload:err});
 
 const token = localStorage.getItem('token');
 
-export const register = (userData) => async (dispatch)=> {
-    dispatch(REGISTER_REQUEST());
+// Thunk action creator
+export const register = (userData) => async (dispatch) => {
+    dispatch(registerRequest()); // Dispatch the action creator function
 
     try {
-        const response = await axios.post(`${API_BASE_URL}/auth/signup`,userData);
+        const response = await axios.post(`${API_BASE_URL}/auth/signup`, userData);
         const user = await response.data;
         if (user.jwt) {
             localStorage.setItem("jwt", user.jwt);
         }
         console.log("user data register", user);
-        dispatch(REGISTER_SUCCESS(user.jwt));
-    }catch (e) {
-        dispatch(REGISTER_FAILURE(e.message));
+        dispatch(registerSuccess(user.jwt)); // Dispatch the action creator function with payload
+    } catch (e) {
+        dispatch(registerFailure(e.message)); // Dispatch the action creator function with payload
     }
 };
 
 export const login = (userData) => async (dispatch)=> {
-    dispatch(LOGIN_REQUEST());
+    dispatch(loginRequest());
 
     try {
         const response = await axios.post(`${API_BASE_URL}/auth/signin`,userData);
@@ -43,14 +45,14 @@ export const login = (userData) => async (dispatch)=> {
             localStorage.setItem("jwt", user.jwt);
         }
         console.log("user data login", user);
-        dispatch(LOGIN_SUCCESS(user.jwt));
+        dispatch(loginSuccess(user.jwt));
     }catch (e) {
-        dispatch(LOGIN_FAILURE(e.message));
+        dispatch(loginFailure(e.message));
     }
 };
 
 export const getUser = () => async (dispatch)=> {
-    dispatch(GET_USER_REQUEST());
+    dispatch(getUserRequest());
 
     try {
         const response = await axios.post(`${API_BASE_URL}/api/users/profile`,{
@@ -60,12 +62,13 @@ export const getUser = () => async (dispatch)=> {
         });
         const user = await response.data;
         console.log("user data", user);
-        dispatch(GET_USER_SUCCESS(user));
+        dispatch(getUserSuccess(user));
     }catch (e) {
-        dispatch(GET_USER_FAILURE(e.message));
+        dispatch(getUserFailure(e.message));
     }
 };
 
 export const logout = () => (dispatch) => {
     dispatch({type:LOGOUT, payload:null});
+    localStorage.clear();
 };
