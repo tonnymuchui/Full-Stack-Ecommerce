1dd77147562f1ee711d0f048ab2ebd439b88d40a
tonnymuchui <tonykanyingah@gmail.com>
2024-02-25 03:13:12 +0300

    Add product Reducer

---
+++ Ecommerce-Frontend/src/State/Products/Reducer.js
@@ -0,0 +1,24 @@
+import { FIND_PRODUCTS_FAILURE, FIND_PRODUCTS_REQUEST, FIND_PRODUCTS_SUCCESS, FIND_PRODUCT_BY_ID_FAILURE, FIND_PRODUCT_BY_ID_REQUEST, FIND_PRODUCT_BY_ID_SUCCESS } from "./ActionType"
+
+const initialState = {
+    products: [],
+    product: null,
+    loading: false,
+    error: null,
+}
+
+export const customerProductReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FIND_PRODUCTS_REQUEST:
+        case FIND_PRODUCT_BY_ID_REQUEST:
+            return {...state, loading: true, error: null}
+        case FIND_PRODUCTS_SUCCESS:
+            return {...state, loading: false, error: null, product: action.payload}
+        case FIND_PRODUCT_BY_ID_SUCCESS:
+            return {...state, loading: false, error: null, product: action.payload}
+        case FIND_PRODUCTS_FAILURE:
+        case FIND_PRODUCT_BY_ID_FAILURE:
+            return {...state, loading: false, error: null}
+        default: return state;
+    }
+}
