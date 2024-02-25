c1190ebbe38867e41e3513d10531c6323d686c00
tonnymuchui <tonykanyingah@gmail.com>
2024-02-25 03:11:35 +0300

    Update actiontype

--- Ecommerce-Frontend/src/State/Auth/ActionType.js
+++ Ecommerce-Frontend/src/State/Auth/ActionType.js
@@ -10,4 +10,4 @@ export const GET_USER_REQUEST = 'GET_USER_REQUEST';
 export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
 export const GET_USER_FAILURE = 'GET_USER_FAILURE';
 
-export const LOGOUT= 'LOGOUT';
+export const LOGOUT = 'LOGOUT';
