59985d6aef0b4f21d02b83eb0b13fadb58469ba4
tonnymuchui <tonykanyingah@gmail.com>
2024-02-26 03:33:58 +0300

    Add Cart Reducer

--- Ecommerce-Frontend/src/State/store.js
+++ Ecommerce-Frontend/src/State/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
 import { customerProductReducer } from "./Products/Reducer";
+import cartReducer from "./Cart/Reducer";
 
 const rootReducers = combineReducers({
     auth: authReducer,
-    products: customerProductReducer
+    products: customerProductReducer,
+    cart:cartReducer
 })
 
 export const store = legacy_createStore(rootReducers,applyMiddleware(thunk));
