870400cbb812c761a4cbe9f38155d999bc24990b
tonnymuchui <tonykanyingah@gmail.com>
2024-02-26 03:33:33 +0300

    Add api config

--- Ecommerce-Frontend/src/config/ApiConfig.js
+++ Ecommerce-Frontend/src/config/ApiConfig.js
@@ -1,1 +1,11 @@
+import axios from "axios";
+
 export const API_BASE_URL = 'http://localhost:5454';
+const jwt = localStorage.getItem('jwt');
+export const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        "Authorization": `Bearer ${jwt.token}`,
+        "Content-Type": "application/json"
+    },
+});
